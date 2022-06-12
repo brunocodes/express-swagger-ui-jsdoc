30e1c7e59d5472dd12bf1dd1db04cb40ddcfa4cf
Bruno Batista <bho360@hotmail.com>
2022-06-12 18:10:27 -0300

    v0.0.1

--- README.md
+++ README.md
@@ -4,12 +4,17 @@
 ```
 git clone https://github.com/brunocodes/express-swagger-ui-jsdoc.git
 ```
+
 * Install
 ```
 cd express-swagger-ui-jsdoc
 npm install
 ```
+
 * Run
 ```
 npm run dev
 ```
+
+* API Documentation
+http://localhost:5000/docs
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification))
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification))
 
 app.use('/users', require('../routes/user'))
 app.use('/auth', require('../routes/auth/auth'))
